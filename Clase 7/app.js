adb3be1ef525c606dda16701d229ed93ef103770
Allan-Figueroa <2985725080101@ingenieria.usac.edu.gt>
2021-04-29 21:50:49 -0600

    Clase 7 (Final)
    Ultimo ejemplo taller Lenguajes

---
+++ Clase 7/app.js
@@ -0,0 +1,11 @@
+document.getElementById("form1").addEventListener("submit", registrarCarro)
+
+function registrarCarro(e){
+    let marca = document.getElementById("marca").value;
+    let modelo = document.getElementById("modelo").value;
+
+    let carro={
+        marca: marca,
+        modelo: modelo,
+    };
+}
